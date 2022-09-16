add7770e740f1862214d7287847a5071205e7499
zeroxx1986 <zeroxx1986@gmail.com>
2022-09-16 18:32:04 +0200

    Update ajtovedo.js

--- ajtovedo.js
+++ ajtovedo.js
@@ -1,1 +1,15 @@
-console.log("HERE AM I!");
+
+import {h, Component, render} from 'https://unpkg.com/preact?module'
+import htm from 'https://unpkg.com/htm?module'
+
+const html = htm.bind(h)
+
+export class Test extends Component {
+  constructor() {
+    super()
+  }
+  
+  render() {
+    return html`<div>Testing</div>`
+  }
+}
